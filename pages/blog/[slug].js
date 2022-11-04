07463903f14f0045f04e253504ab84a916376ad9
Paul Caine <paulcaine@tutanota.com>
2022-11-04 15:39:10 +0000

    added delete button

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,22 @@
 import { supabase } from "../../utils/supabase";
 
+import Head from "next/head";
+
 export default function SingleBlog({ blog }) {
   return (
     <div className="max-w-4xl m-auto mt-10">
-      <div className="first-letter:text-purple-500 text-2xl capitalize border-b-2 border-l-8 pl-3 pr-10 max-w-fit">
+      <Head>
+        <title>Testing | {blog.title}</title>
+      </Head>
+      <div className="text-2xl capitalize border-l-8 border-l-purple-700 pl-3 pr-10 max-w-fit">
         {blog.title}
       </div>
 
       <div className="my-10">{blog.body}</div>
+
+      <button className="py-2 px-3 rounded-md bg-red-600 text-gray-50 font-bold shadow-lg hover:bg-red-800 transition-all">
+        Delete Post
+      </button>
     </div>
   );
 }
